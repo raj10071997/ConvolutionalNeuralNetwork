ec8e50c49715e6edbb3f99fc3a059a266ef9b75a
raj10071997 <sahudhanraj123@gmail.com>
2018-10-26 18:00:20 +0530

    added with numerical gradient descent SVM classifier

--- SVM.py
+++ SVM.py
@@ -42,6 +42,70 @@ def L(X,Y,W):
 	return loss	
 
 
+def CIFAR10_loss_fun(W):
+	return L(X_train,Y_train,W)
+
+
+# instead of evaluating on the whole dataset for updating one 
+#parameter use mini batch gradient descent and use calculus instead of numerical approach
+def eval_numerical_gradient(f,W):
+	fx = f(W)
+	W1 = W
+	W2 = W
+	h = 0.0001
+	grad = np.zeros(W.shape)
+	it = np.nditer(W, flags=['multi_index'], op_flags=['readwrite'])
+	while not it.finished:
+		idx = it.multi_index
+		old_value = W[idx]
+		W1[idx] += h
+		W2[idx] -= h
+		fx_new1 = f(W1)
+		fx_new2 = f(W2)
+		grad[idx] = (fx_new1-fx_new2)/(2*h)
+		W1[idx] = old_value
+		W2[idx] = old_value
+		it.iternext()
+
+	return grad
+
+# W = (10,3073) X_test = (3073,10000), 3073 not 3072 because of the bias
+def predict(W,X_test):
+	scores = W.dot(X_test)
+	scores.transpose()
+	Y_pred = np.zeros(X_test.shape[1])
+	for i in range(X_test.shape[1]):
+		test_scores_for_each_image = scores[i]
+		Y_pred[i] = np.argmax(test_scores_for_each_image)
+
+	return Y_pred
+
+W = np.random.rand(10, 3073) * 0.001
+df = eval_numerical_gradient(CIFAR10_loss_fun, W)
+
+best_loss = CIFAR10_loss_fun(W)
+best_weights = W
+
+for steps in [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]:
+	step_size = 10 ** steps
+	W_new = W - step_size*df
+	new_loss = CIFAR10_loss_fun(W_new)
+	if new_loss < best_loss:
+		best_loss = new_loss
+		best_weights = W
+
+
+
+predictions = predict(best_weights, X_test)
+
+print(np.mean(predictions == Y_test))
+
+
+
+
+
+
+
 
 
 
